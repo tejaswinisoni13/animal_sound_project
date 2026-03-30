1992e07a1102b8cb8d924fcb8b8de9da5a6be83d
tejaswinisoni13 <tejaswinisonisgsits29@gmail.com>
2026-03-30 17:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,5 +283,8 @@ def index():
 # =========================
 # RUN APP
 # =========================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
